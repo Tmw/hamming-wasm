4e8912cb6176bb5e929acf2e3411b7662fd8e58f
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2020-12-30 12:16:20 +0100

    Run text through hamming encode / decode

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,19 @@ use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 use web_sys::HtmlElement;
 
+use hamming::blocks::Blocks;
+
 mod rendering_mode;
 use rendering_mode::RenderingMode;
 
 struct Model {
     link: ComponentLink<Self>,
-    value: String,
+    input_string: String,
+    output_string: String,
     bits: Vec<bool>,
     input_ref: NodeRef,
     rendering_mode: RenderingMode,
 }
-
 
 enum Msg {
     Update(String),
@@ -27,7 +29,8 @@ impl Component for Model {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
-            value: "".into(),
+            input_string: "".into(),
+            output_string: "".into(),
             bits: vec![],
             input_ref: NodeRef::default(),
             rendering_mode: RenderingMode::Sequential,
@@ -37,8 +40,10 @@ impl Component for Model {
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::Update(val) => {
-                self.value = val;
-                self.set_bits();
+                self.input_string = val.clone();
+                self.output_string = val.clone();
+                self.hamming_encode();
+                self.hamming_decode();
             },
 
             Msg::ToggleMode => {
@@ -86,7 +91,7 @@ impl Component for Model {
                     <h2>{ "Output" }</h2>
                     <textarea
                         placeholder="As you type, your data will appear here.."
-                        value=self.value
+                        value=self.output_string
                         disabled=true
                     />
                 </div>
@@ -101,6 +106,9 @@ impl Component for Model {
     }
 }
 
+type Bytes = Vec<u8>;
+type Bits = Vec<bool>;
+
 impl Model {
     fn set_focus(&mut self) {
         if let Some(input) = self.input_ref.cast::<HtmlElement>() {
@@ -108,9 +116,20 @@ impl Model {
         }
     }
 
-    fn set_bits(&mut self) {
-        let bytes = self.value.as_bytes();
-        let mut bits: Vec<bool> = Vec::new();
+    fn hamming_encode(&mut self) {
+        let blocks = Blocks::new(&self.input_string.as_bytes(), false);
+        self.bits = Model::bytes_to_bits(&blocks.to_byte_vec());
+    }
+
+    fn hamming_decode(&mut self) {
+        let bytes = Model::bits_to_bytes(&self.bits);
+        let mut blocks = Blocks::new(&bytes, true);
+        blocks.repair();
+        self.output_string = blocks.to_string();
+    }
+
+    fn bytes_to_bits(bytes: &Bytes) -> Bits {
+        let mut bits: Bits = Vec::new();
 
         for byte in bytes {
             for bit in 0..8 {
@@ -118,7 +137,24 @@ impl Model {
             }
         }
 
-        self.bits = bits;
+        bits
+    }
+
+    fn bits_to_bytes(bits: &Bits) -> Bytes {
+        let mut bytes: Bytes = Vec::new();
+
+        for chunk in bits.chunks(8) {
+            let byte = chunk.iter().enumerate().fold(0_u8, |acc, (idx, b)| {
+                match b {
+                    true => acc | 0b1 << idx,
+                    false => acc | 0b0 << idx
+                }
+            });
+
+            bytes.push(byte);
+        }
+
+        bytes
     }
 
     fn view_bit(bit: bool) -> Html {
