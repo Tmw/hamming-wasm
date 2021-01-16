4c1fe77fac5052c17bb5e06114d224f901c438ad
Tmw <Tmw@users.noreply.github.com>
2021-01-16 12:55:25 +0000

    deploy: d900c6199788d653bd4b4f9a9ff7d55f8e32cbcb

--- wasm.js
+++ wasm.js
@@ -208,7 +208,7 @@ function addBorrowedObject(obj) {
 }
 function __wbg_adapter_20(arg0, arg1, arg2) {
     try {
-        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc7ef7305480f1f6e(arg0, arg1, addBorrowedObject(arg2));
+        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hdfdd3fdba87ccec5(arg0, arg1, addBorrowedObject(arg2));
     } finally {
         heap[stack_pointer++] = undefined;
     }
